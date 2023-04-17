e1ec966ff940d91e88d4c9f3d44fb835a8de80fd
Matthew Riley <matthew.riley@barren.kyschool.us>
2023-04-17 00:36:44 -0500

    Initial Commit

---
+++ GuessThatFlag/GuessThatFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessThatFlagApp.swift
+//  GuessThatFlag
+//
+//  Created by Matthew RIley on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessThatFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
